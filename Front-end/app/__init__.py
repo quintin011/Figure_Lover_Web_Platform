6fc467334171ebc1097dff289bab603ec82e25dc
quintin011 <ec2-user@ip-172-31-80-200.ec2.internal>
2018-10-22 21:41:46 +0000

    updated Web

---
+++ Front-end/app/__init__.py
@@ -0,0 +1,25 @@
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+from flask import Flask, request, current_app
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_babel import Babel, lazy_gettext as _l
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy()
+migrate = Migrate()
+login = LoginManager()
+login.login_view = 'auth.login'
+login.login_message = _l('Please log in to access this page.')
+mail = Mail()
+bootstrap = Bootstrap(app)
+moment = Moment()
+babel = Babel()
+
--- Front-end/run.py
+++ Front-end/run.py
@@ -1,4 +1,4 @@
-from app import create_app
+from app import app
 
 if __name__ == '__main__':
-    create_app().run()
+    app.run(host='0.0.0.0',port = 8081 ,  debug=True)
