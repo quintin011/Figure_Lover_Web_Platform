632777fa1c7efc078dab14bc9570ad0b6cb2a0bf
quintin011 <quintin011011@gmail.com>
2019-01-05 12:19:26 -0500

    third commit

--- test/app/models.py
+++ test/app/models.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 from hashlib import md5
-from app import db, login
 from time import time
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
--- test/config.py
+++ test/config.py
@@ -12,7 +12,7 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['quintin011011@gmail.com']
+    ADMINS = ['your-email@example.com']
     POSTS_PER_PAGE = 25
     
     
